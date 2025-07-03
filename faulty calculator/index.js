74232fbd941c4ff70459bb850c98a3d1f2e0515c
Jarvis612 <amitsarkar6124@gmail.com>
2025-07-03 22:48:04 +0530

    faulty calculator backend done
    front end may i develop later

---
+++ faulty calculator/index.js
@@ -0,0 +1,40 @@
+/* Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It perfoms wrong operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+It performs wrong operation 10% of the times
+
+*/
+
+
+let random = Math.random();
+let a = prompt("Enter the first number")
+let c = prompt("Enter the operation")
+let b = prompt("Enter the second number")
+
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**",
+}
+
+if(random>0.1){
+    // perform correct calculation
+
+    console.log(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+
+}
+else{
+    // perfrom wrong calculation
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
